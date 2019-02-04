ca5d9e1e27a0c1a36829769599f30e479fbe05fc
Jim Persson <jim@nurd.se>
2019-02-05 00:22:59 +0100

    [CPP] Support for printing AST to console

--- pycodegen/frontend/cpp/__init__.py
+++ pycodegen/frontend/cpp/__init__.py
@@ -5,6 +5,7 @@ from .parser_libclang import ParserLibClang
 
 def register_arguments(argument_parser):
     argument_parser.add_argument("--args", nargs=argparse.REMAINDER, help="Arguments to pass to clang")
+    argument_parser.add_argument("--print-ast", action="store_true", default=False, help="Print AST to console")
 
 
 def run(filename, options=None):
@@ -12,4 +13,8 @@ def run(filename, options=None):
         options = {}
 
     parser = ParserLibClang()
+
+    if options.get('print_ast'):
+        print(parser.dump(filename, options.get('args')))
+
     return parser.parse(filename, options.get('args'))
