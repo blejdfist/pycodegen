d29253de35c38dec6e0031e9c5e547f4a9f80f33
Jim Persson <jim@nurd.se>
2019-02-14 18:33:44 +0100

    Remove unused Python2 code

--- pycodegen/core.py
+++ pycodegen/core.py
@@ -11,15 +11,10 @@ def load_driver(driver_filename):
         _LOGGER.error("Driver does not exist")
         return
 
-    if sys.version_info >= (3, 1):
-        import importlib.util
-        spec = importlib.util.spec_from_file_location(driver_filename, driver_filename)
-        driver_module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(driver_module)
-        return driver_module
-    else:
-        import imp
-        return imp.load_source("module", driver_filename)
+    spec = importlib.util.spec_from_file_location(driver_filename, driver_filename)
+    driver_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(driver_module)
+    return driver_module
 
 
 def get_frontend_by_name(name):
