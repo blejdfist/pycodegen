a7720da8e171b11c372c40c96b87d1732556300a
Jim Ekman <jim.ekman@here.com>
2026-05-07 11:48:20 +0200

    Drop python 3.9

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import nox
 
 nox.options.default_venv_backend = "uv"
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13", "3.14"])
+@nox.session(python=["3.10", "3.11", "3.12", "3.13", "3.14"])
 def tests(session):
     session.install(".[cpp]", "pytest")
     session.run("pytest")
